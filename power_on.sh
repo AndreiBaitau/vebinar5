eaaa86a187950629af044cb57b4831399d3a3fd9
AndreiBaitau <baitovandrei@gmail.com>
2022-08-15 11:41:38 +0300

    Power on YOUR PC

---
+++ power_on.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+sqlite3=`which sqlite3`
+DB_FILE=~/vebinar5.db
+SQLITE_OPTIONS=" -column -header "
+random=`echo $(($RANDOM % 3))`
+stat=`$sqlite3 $DB_FILE "select status from computer order by resource DESC LIMIT 1;"`  
+res=`$sqlite3 $DB_FILE "select resource from computer order by resource DESC LIMIT 1;"`
+
+off="Off"
+on="On"
+killed="Killed"
+value=$1
+ 
+
+function boot(){
+ 
+if [ $random -eq $value ]
+then
+ $sqlite3 $SQLITE_OPTIONS $DB_FILE "insert into computer values ('"On"',null);"
+ echo "PC is successfuly powered ON!"
+else
+ $sqlite3 $DB_FILE "insert into computer values ('"Killed"',null);"
+ echo "You killed PC"
+fi
+}
+
+
+
+
+
+function check(){
+if [ "$stat" = $off ]
+then
+ echo "Checking your value"
+ boot
+elif [ "$stat" = $on ] 
+then
+ echo "Your PC is already powered ON"
+  exit 0
+else 
+ echo "Your PC is KILLED"
+ exit 0
+fi
+}
+
+if [ "$random" -eq 2 ]
+then
+ echo " Kernel PANIC "
+ exit 0
+else
+ echo "Processing..."
+fi
+
+if [ $res -gt 10 ]
+then 
+ echo "The resource is exhausted"
+ exit 0
+elif [ "$stat" = "" ] 
+then 
+ echo "Checking your value"
+ boot
+else
+ check
+fi
+
+echo "Random value was $random"
